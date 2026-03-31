fecbb324b8e42aa3e34a4a945625a7103653a063
itsuki <itsuki.enjyoy@gmail.com>
2026-03-31 14:01:58 +0900

    Initial Commit

---
+++ GetContainerAppState/GetContainerAppStateApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetContainerAppStateApp.swift
+//  GetContainerAppState
+//
+//  Created by Itsuki on 2026/03/31.
+//
+
+import SwiftUI
+
+@main
+struct GetContainerAppStateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
